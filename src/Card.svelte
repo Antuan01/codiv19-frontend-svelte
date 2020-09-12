a67eb5a9b245e62bf4ee877c942d629e2940d4ea
antuan agreda <antuan@antuan.labs>
2020-09-12 09:14:11 -0400

    test request

--- src/Card.svelte
+++ src/Card.svelte
@@ -1,18 +1,15 @@
 <script>
 
-  //   onMount(async () => {
-		
-	// });
+  import axios from "axios";
+  import { onMount } from 'svelte';
 
-  //   onDestroy(() => );
+function getData() {
+      let data = axios.get('https://api.covid19api.com/country/spain?from=2020-09-01T00:00:00Z&to=2020-09-02T00:00:00Z')
+          .then(res => console.log(res.data))
+          .catch(err => console.log(err));
+}
 
-  //   beforeUpdate(() => {
-
-  //   });
-
-  //   afterUpdate(() => {
-        
-  //   });
+onMount(() => getData());
 
 </script>
 
@@ -21,16 +18,16 @@
 </style>
 
 <div class="max-w-sm rounded overflow-hidden shadow-lg">
-  <!-- <img class="w-full" src="/img/card-top.jpg" alt="Sunset in the mountains"> -->
-  <div class="px-6 py-4">
-    <div class="font-bold text-xl mb-2">Una Card de Tailwind en Svelte </div>
-    <p class="text-gray-700 text-base">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
-    </p>
-  </div>
-  <div class="px-6 pt-4 pb-2">
-    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
-  </div>
+<!-- <img class="w-full" src="/img/card-top.jpg" alt="Sunset in the mountains"> -->
+<div class="px-6 py-4">
+  <div class="font-bold text-xl mb-2">Una Card de Tailwind en Svelte </div>
+  <p class="text-gray-700 text-base">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+  </p>
+</div>
+<div class="px-6 pt-4 pb-2">
+  <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+  <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+  <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+</div>
 </div>
