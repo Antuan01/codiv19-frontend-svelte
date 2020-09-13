b702216909e31aa7495c3440bba0f6e94202628b
Samuel Silva <sdjsz7187@gmail.com>
2020-09-13 18:52:34 -0400

    show 8 cards random to test

--- src/store/stores.js
+++ src/store/stores.js
@@ -8,8 +8,8 @@ export const countries = writable([]);
 export const countriesSelectedRandomly = derived(
     countries,
     $countries => {
-        const rand = getRandomInt($countries.length - 5)
-        return $countries.slice(rand, rand + 4)
+        const rand = getRandomInt($countries.length - 9)
+        return $countries.slice(rand, rand + 8)
     }
 );
 //handle single countries
