f5748713dbf42a1c05d6243f7999a8498e3aed46
Samuel Silva <sdjsz7187@gmail.com>
2020-09-13 18:52:21 -0400

    css updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   purge: ["./src/**/*.svelte", "./src/**/*.html"],
   theme: {
-    extend: {},
+    maxWidth: {
+      "400": "400px"
+    },
+    extend: {
+      width: {
+        "9/10": "90%"
+      }
+    },
   },
   variants: {},
   plugins: [],
