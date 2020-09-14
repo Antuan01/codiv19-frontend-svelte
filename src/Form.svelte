73457262b11fff4da6ba1afeef053ecfec20c1f4
antuan agreda <antuan@antuan.labs>
2020-09-14 16:33:01 -0400

    validation errors

--- src/Form.svelte
+++ src/Form.svelte
@@ -8,13 +8,22 @@ let values = {
   number: '',
 };
 
-const checkFields = () => FormSchema.validate(values, { abortEarly: false })
+let errors = [];
+
+const checkFields = () => {
+      errors = []
+    FormSchema.validate(values, { abortEarly: false })
     .then(() => {
       alert(JSON.stringify(values, null, 2));
     })
     .catch((err) => {
       alert(JSON.stringify(err.message, null, 2));
-    });;
+      console.log(err.inner)
+      console.log(err.inner.map(el => console.log(el.path)))
+      err.inner.map(el => errors = [el.path,...errors])
+    });
+
+  }
 
 </script>
 
@@ -25,12 +34,18 @@ const checkFields = () => FormSchema.validate(values, { abortEarly: false })
           Name
         </label>
         <input 
-        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        class:border-red-500={errors.includes('name')}
         id="name" 
         type="text" 
         placeholder=""
         bind:value={values.name}
         >
+        {#if errors.includes('name')}
+          <p class="text-red-500">
+              The name is required
+          </p>
+        {/if}
       </div>
       <div class="mb-6">
         <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
@@ -42,7 +57,13 @@ const checkFields = () => FormSchema.validate(values, { abortEarly: false })
         type="email" 
         placeholder=""
         bind:value={values.email}
+        class:border-red-500={errors.includes('email')}
         >
+        {#if errors.includes('email')}
+        <p class="text-red-500">
+            Please enter a valid email
+        </p>
+      {/if}
       </div>
       <div class="mb-6">
         <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
@@ -53,7 +74,13 @@ const checkFields = () => FormSchema.validate(values, { abortEarly: false })
         type="password" 
         placeholder=""
         bind:value={values.password}
+        class:border-red-500={errors.includes('password')}
         >
+        {#if errors.includes('password')}
+        <p class="text-red-500">
+            The password field is required
+        </p>
+      {/if}
       </div>
       <div class="mb-6">
         <label class="block text-gray-700 text-sm font-bold mb-2" for="number">
@@ -65,7 +92,13 @@ const checkFields = () => FormSchema.validate(values, { abortEarly: false })
         type="number" 
         placeholder=""
         bind:value={values.number}
+        class:border-red-500={errors.includes('number')}
         >
+        {#if errors.includes('number')}
+        <p class="text-red-500">
+            The number field is requiered
+        </p>
+      {/if}
       </div>
       <div class="flex items-center justify-between">
         <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
