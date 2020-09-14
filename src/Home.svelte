68005f9f1f36f19d789620bb02258278b8f96fff
Samuel Silva <sdjsz7187@gmail.com>
2020-09-14 15:29:43 -0400

    identation app/home

--- src/Home.svelte
+++ src/Home.svelte
@@ -1,17 +1,14 @@
 <script>
     import CardsContainer from "./CardsContainer.svelte";
-    import axios from 'axios';
+    import axios from "axios";
 
-    axios.defaults.baseURL = 'https://api.covid19api.com/';
+    axios.defaults.baseURL = "https://api.covid19api.com/";
 </script>
-<div class="w-full flex justify-center flex-wrap" >
+
+<main class="w-full flex justify-center flex-wrap">
     <div class="w-9/10 pt-8 pb-6">
-        <h1 class="text-2xl">
-            Covid-19 Dashboard
-        </h1>
+        <h1 class="text-2xl">Covid-19 Dashboard</h1>
         <p>from September 1, 2020 until today</p>
     </div>
-    <CardsContainer/>
-</div>
-
-
+    <CardsContainer />
+</main>
