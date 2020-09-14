d3547d26bc38024b62963c39046e8aba47e14ae1
Samuel Silva <sdjsz7187@gmail.com>
2020-09-14 18:07:17 -0400

    styles added, and fix tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,21 @@
 module.exports = {
   purge: ["./src/**/*.svelte", "./src/**/*.html"],
   theme: {
-    maxWidth: {
-      "400": "400px"
-    },
     extend: {
       width: {
         "9/10": "90%"
-      }
+      },
+      margin: {
+        "5%": "5%"
+      },
+      minHeight: {
+        "180": 180
+      },
+      maxWidth: {
+        "400": "400px",
+        "630": "630px"
+      },
     },
-    minHeight: {
-      "180": 180
-    }
   },
   variants: {},
   plugins: [],
