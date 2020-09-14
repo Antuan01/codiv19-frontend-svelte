7f9fd28212cc5bece7970c71d84f749e5dc4d507
Samuel Silva <sdjsz7187@gmail.com>
2020-09-14 15:29:06 -0400

    container component to loader or error in dashboard

---
+++ src/DashboardContainer.svelte
@@ -0,0 +1,9 @@
+<style>
+    .min-height-loading-error {
+        min-height: 70vh;
+    }
+</style>
+
+<div class="min-height-loading-error w-full flex justify-center items-center">
+    <slot />
+</div>
