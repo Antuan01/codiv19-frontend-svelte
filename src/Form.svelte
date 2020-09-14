2a4f75c39dc8bb9758cf46f1ed84f0c5aaddbf53
Samuel Silva <sdjsz7187@gmail.com>
2020-09-14 18:24:31 -0400

    merge

--- src/Form.svelte
+++ src/Form.svelte
@@ -1,31 +1,29 @@
 <script>
-  import { FormSchema } from './validation/FormSchema';
+    import {FormSchema} from "./validation/FormSchema";
     import Button from "./Button.svelte";
     export let countryName;
-let values = {
-  name: '',
-  email: '',
-  password: '',
-  number: '',
-};
+    let values = {
+        name: "",
+        email: "",
+        password: "",
+        number: "",
+    };
 
-let errors = [];
+    let errors = [];
 
-const checkFields = () => {
-      errors = []
-    FormSchema.validate(values, { abortEarly: false })
-    .then(() => {
-      alert(JSON.stringify(values, null, 2));
-    })
-    .catch((err) => {
-      alert(JSON.stringify(err.message, null, 2));
-      console.log(err.inner)
-      console.log(err.inner.map(el => console.log(el.path)))
-      err.inner.map(el => errors = [el.path,...errors])
-    });
-
-  }
-
+    const checkFields = () => {
+        errors = [];
+        FormSchema.validate(values, {abortEarly: false})
+            .then(() => {
+                alert(JSON.stringify(values, null, 2));
+            })
+            .catch(err => {
+                alert(JSON.stringify(err.message, null, 2));
+                console.log(err.inner);
+                console.log(err.inner.map(el => console.log(el.path)));
+                err.inner.map(el => (errors = [el.path, ...errors]));
+            });
+    };
 </script>
 
 <div class="w-full">
@@ -34,83 +32,72 @@ const checkFields = () => {
         <div class="mb-6">
             <label class="block text-sm font-bold mb-2" for="confirmed"> Confirmed </label>
             <input
-        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-        class:border-red-500={errors.includes('name')}
-        id="name" 
-        type="text" 
-        placeholder=""
-        >
-        bind:value={values.name}
-        {#if errors.includes('name')}
-          <p class="text-red-500">
-              The name is required
-          </p>
-        {/if}
-      </div>
-      <div class="mb-6">
-        <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
-          Email
-        </label>
-        <input 
-        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" 
-        id="email" 
-        type="email" 
-        placeholder=""
-        bind:value={values.email}
-        class:border-red-500={errors.includes('email')}
-        >
-        {#if errors.includes('email')}
-        <p class="text-red-500">
-        </p>
-      {/if}
-      </div>
-      <div class="mb-6">
-        <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
-        </label>
-          Password
-        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
-        id="password" 
-        type="password" 
-        placeholder=""
-        bind:value={values.password}
-        class:border-red-500={errors.includes('password')}
-        >
-        {#if errors.includes('password')}
-        <p class="text-red-500">
-        </p>
-            The password field is required
-      {/if}
-      </div>
-      <div class="mb-6">
-        <label class="block text-gray-700 text-sm font-bold mb-2" for="number">
-          Number
-        </label>
-        <input 
-        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" 
-        id="number" 
-        type="number" 
-        placeholder=""
-        bind:value={values.number}
-        class:border-red-500={errors.includes('number')}
-        >
-        {#if errors.includes('number')}
-        <p class="text-red-500">
-            The number field is requiered
-        </p>
-      {/if}
-      </div>
-      <div class="flex items-center justify-between">
-        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-        type="button"
-        on:click={checkFields}
-        >
-          Submit
-        </button>
-      </div>
+                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight
+                    focus:outline-none focus:shadow-outline"
+                class:border-red-500={errors.includes('name')}
+                id="name"
+                type="text"
+                placeholder=""
+                bind:value={values.name} />
+            {#if errors.includes('name')}
+                <p class="text-red-500">The name is required</p>
+            {/if}
+        </div>
+        <div class="mb-6">
+            <label class="block text-sm font-bold mb-2" for="email"> Email </label>
+            <input
+                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3
+                    leading-tight focus:outline-none focus:shadow-outline"
+                id="email"
+                type="email"
+                placeholder=""
+                bind:value={values.email}
+                class:border-red-500={errors.includes('email')} />
+            {#if errors.includes('email')}
+                <p class="text-red-500" />
+            {/if}
+        </div>
+        <div class="mb-6">
+            <label class="block text-gray-700 text-sm font-bold mb-2" for="password" /> Password <input
+                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight
+                    focus:outline-none focus:shadow-outline"
+                id="password"
+                type="password"
+                placeholder=""
+                bind:value={values.password}
+                class:border-red-500={errors.includes('password')} />
+            {#if errors.includes('password')}
+                <p class="text-red-500" />
+                The password field is required
+            {/if}
+        </div>
+        <div class="mb-6">
+            <label class="block text-sm font-bold mb-2" for="number"> Number </label>
+            <input
+                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3
+                    leading-tight focus:outline-none focus:shadow-outline"
+                id="number"
+                type="number"
+                placeholder=""
+                bind:value={values.number}
+                class:border-red-500={errors.includes('number')} />
+            {#if errors.includes('number')}
+                <p class="text-red-500">The number field is requiered</p>
+            {/if}
+        </div>
+        <div class="flex items-center justify-between">
+            <button
+                class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded
+                    focus:outline-none focus:shadow-outline"
+                type="button"
+                on:click={checkFields}>
+                Submit
+            </button>
+        </div>
     </form>
 
     <!-- <div> -->
-  <!-- {#await result}
+    <!-- {#await result}
       <p> loading </p>
   {:then value}
     <h2>Validation Result</h2>
@@ -121,8 +108,5 @@ const checkFields = () => {
   {/await}
 </div> -->
 
-    <p class="text-center text-gray-500 text-xs">
-      &copy;2020 Codiv Corp. All rights reserved.
-    </p>
-  </div>
-  </div>
+    <p class="text-center text-gray-500 text-xs">&copy;2020 Codiv Corp. All rights reserved.</p>
+</div>
--- src/Home.svelte
+++ src/Home.svelte
@@ -1,7 +1,7 @@
 <script>
     import CardsContainer from "./CardsContainer.svelte";
     import Form2 from "./Form2.svelte";
-    import axios from 'axios';
+    import axios from "axios";
 
     axios.defaults.baseURL = "https://api.covid19api.com/";
 </script>
@@ -11,6 +11,6 @@
         <h1 class="text-2xl">Covid-19 Dashboard</h1>
         <p>from September 1, 2020 until today</p>
     </div>
-    <CardsContainer/>
-    <Form2/>
+    <CardsContainer />
+    <!-- <Form2/> -->
 </main>
